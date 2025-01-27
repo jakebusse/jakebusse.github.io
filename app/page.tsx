d904941154201c75f0c0d9dce730ff4cbc122922
Jake Busse <78458447+jakebusse@users.noreply.github.com>
2025-01-27 08:53:03 -0600

    homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,132 @@
+"use client";
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+import React, { useState } from "react";
+import Link from "next/link";
+import { GoChevronDown, GoChevronRight } from "react-icons/go";
+import { FiLinkedin, FiGithub } from "react-icons/fi";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const items = [
+    { text: "IT Professional", url: "/it" },
+    { text: "Developer", url: "/dev" },
+    { text: "Photographer", url: "/photos" },
+    { text: "Chef", url: "/recipes" },
+  ];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const [isAnimating, setIsAnimating] = useState(true);
+
+  const [currentItem, setCurrentItem] = useState<{ text: String; url: string }>(
+    items[0]
+  );
+
+  const [selectedItem, setSelectedItem] = useState<{
+    text: String;
+    url: string;
+  }>({
+    text: "null",
+    url: "null",
+  });
+
+  const stopAnimationAndSelect = (index: number) => {
+    setIsAnimating(false);
+    setCurrentItem(items[index]);
+    setSelectedItem(items[index]);
+    setSelectedIndex(index);
+    setIsOpen(false); // Close the dropdown
+  };
+
+  const socials = [
+    {
+      name: "LinkedIn",
+      icon: <FiLinkedin />,
+      url: "https://linkedin.com/in/jakebusse",
+    },
+    {
+      name: "GitHub",
+      icon: <FiGithub />,
+      url: "https://github.com/jakebusse",
+    },
+  ];
+
+  return (
+    <div className="h-screen w-screen flex flex-col gap-8 items-center justify-center content-center text-center">
+      <div className="absolute top-5 right-5 flex flex-row flex-nowrap gap-4">
+        {socials.map((social) => (
+          <Link
+            href={social.url}
+            key={social.name}
+            className="transition-all ease-in bg-gray-200 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600 p-2 rounded-full"
             target="_blank"
-            rel="noopener noreferrer"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+            {social.icon}
+          </Link>
+        ))}
+      </div>
+      <h1 className="text-6xl font-medium">A Jake of all trades.</h1>
+      <span className="text-4xl relative">
+        {"Jake the "}
+        <span
+          className="border-b-4 cursor-pointer block md:inline"
+          onClick={() => {
+            setIsAnimating(false); // Stop animation
+            setIsOpen(!isOpen); // Open or close dropdown
+            if (isAnimating) {
+              setSelectedItem(currentItem);
+            }
+          }}
+        >
+          {isAnimating ? (
+            <TypeAnimation
+              sequence={[
+                // Cycle through each word
+                ...items.map((item) => [
+                  () => {
+                    if (isAnimating) setCurrentItem(item); // Only update if animation is active
+                  },
+                  item.text,
+                  2000, // Display duration for each word
+                ]),
+              ].flat()}
+              wrapper="span"
+              speed={1}
+              repeat={Infinity}
+              cursor={true}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          ) : selectedItem.text != "null" ? (
+            selectedItem.text
+          ) : (
+            currentItem.text
+          )}
+          &nbsp;
+          <GoChevronDown className="inline" />
+        </span>
+        {/* Dropdown Menu */}
+        {isOpen && (
+          <ul className="absolute right-0 w-auto bg-white dark:bg-gray-800 border border-t-4 rounded-sm z-10 float-right">
+            {items.map((item, index) => (
+              <li
+                key={index}
+                className={`px-6 py-3 cursor-pointer border-b border-t border-gray-100 hover:bg-gray-100 dark:hover:bg-gray-900 ${
+                  selectedIndex === index ? "hidden" : "block"
+                }`}
+                onClick={() => stopAnimationAndSelect(index)}
+              >
+                {item.text}
+              </li>
+            ))}
+          </ul>
+        )}
+      </span>
+      <Link
+        href={selectedItem.text != "null" ? selectedItem.url : currentItem.url}
+        className="bg-gray-600 text-white rounded p-4 cursor-pointer hover:bg-gray-500 dark:hover:bg-gray-700 transition-all ease-linear"
+      >
+        Go <GoChevronRight className="inline" />
+      </Link>
     </div>
   );
 }
