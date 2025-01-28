9aa90f4df5e49fb1510c4d3cd3d9df48ff2fb430
Jake Busse <jakerbusse@gmail.com>
2025-01-28 13:33:53 -0600

    clean up code

--- app/it/components/window/window.tsx
+++ app/it/components/window/window.tsx
@@ -1,66 +1,70 @@
 "use client";
+
 import "./window.css";
 import React from "react";
 import { useDraggable } from "@dnd-kit/core";
 import { CSS } from "@dnd-kit/utilities";
 import MyComputer from "../../windows/my-computer";
-import Home from '../../../page';
+import Home from "../../../page";
 
-type ChildProps = {
+type WindowProps = {
   name: string;
   icon: string;
   id: string;
   open: boolean;
   active: boolean;
-  makeActive: (value: React.Dispatch<React.SetStateAction<string>>) => void;
+  makeActive: () => void;
   position: {
-    x: number,
-    y: number
-  }
+    x: number;
+    y: number;
+  };
 };
 
-export default function Window({ name, icon, id, open, active, makeActive, position }:ChildProps) {
+export default function Window({
+  name,
+  icon,
+  id,
+  active,
+  makeActive,
+  position,
+}: WindowProps) {
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id,
   });
 
-  // Use CSS for transform to dynamically adjust the draggable position
-  const style = transform
-    ? {
-        transform: CSS.Translate.toString(transform),
-        position: "absolute", // Make the window movable
-        top: position.y, // Default start position
-        left: position.x,
-      }
-    : {
-        position: "absolute",
-        top: position.y,
-        left: position.x,
-      };
+  const style = {
+    transform: transform ? CSS.Translate.toString(transform) : undefined,
+    position: "absolute",
+    top: position.y,
+    left: position.x,
+    zIndex: active ? 50 : 40, // Simplify z-index logic
+  };
+
+  const renderContent = () => {
+    if (id === "my-computer") return <MyComputer />;
+    if (id === "main-site") return <Home />;
+    return null;
+  };
 
   return (
     <div
-      className={`window container ${active ? "z-50" : "z-40"}`}
+      className="window container"
       ref={setNodeRef}
       style={style}
       id={id}
       onClick={makeActive}
     >
-      <div className="windowHeader" {...listeners}
-      {...attributes}>
-        <div className="flex flex-row flex-no-wrap gap-1 items-center">
+      <div className="windowHeader" {...listeners} {...attributes} >
+        <div className="flex flex-row gap-1 items-center">
           <img
-            src={"./icons/" + icon}
+            src={`./icons/${icon}`}
             alt={name}
             className="windowHeaderIcon"
           />
           <span className="windowHeaderTitle">{name}</span>
         </div>
       </div>
-      <div className="windowBody">
-        {id === "my-computer" ? <MyComputer /> : ""}
-        {id === 'main-site' ? <Home /> : ""}
-      </div>
+      <div className="windowBody overflow-hidden">{renderContent()}</div>
     </div>
   );
 }
--- app/it/page.tsx
+++ app/it/page.tsx
@@ -1,73 +1,71 @@
 "use client";
+
 import "./xp.css";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import DesktopShortcut from "./components/desktop-shortcut/desktop-shortcut";
 import Window from "./components/window/window";
 import { DndContext, closestCenter } from "@dnd-kit/core";
-import { restrictToParentElement } from "@dnd-kit/modifiers";
+import { restrictToParentElement, restrictToWindowEdges } from "@dnd-kit/modifiers";
 
-const windowsData = [
+const initialWindows = [
   {
     id: "my-computer",
     name: "My Computer",
     icon: "about_jake.png",
     open: false,
-    position: {
-      x: 100,
-      y: 100
-    }
+    position: { x: 100, y: 100 },
   },
   {
     id: "main-site",
     name: "Jake Busse",
     icon: "cursor.png",
     open: false,
-    position: {
-      x: 50,
-      y: 50
-    }
+    position: { x: 50, y: 50 },
   },
-  
-]
+];
 
 export default function Home() {
   const [time, setTime] = useState(new Date());
   const [selectedDesktopShortcut, setSelectedDesktopShortcut] = useState("");
+  const [windows, setWindows] = useState(initialWindows);
+  const [activeWindow, setActiveWindow] = useState("my-computer");
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
+    const interval = setInterval(() => setTime(new Date()), 1000);
     return () => clearInterval(interval);
   }, []);
 
-  const retVal = time.toLocaleString("en-US", {
+  const formattedTime = time.toLocaleString("en-US", {
     timeStyle: "short",
     hour12: true,
   });
 
-  const [windows, setWindows] = useState(windowsData);
-  const [activeWindow, setActiveWindow] = useState('my-computer');
-
   const handleDragEnd = (event) => {
-    const window = windows.find((x) => x.id === event.active.id);
-    if(window) {
-      window.position.x += event.delta.x;
-      window.position.y += event.delta.y;
-      const newWindows = windows.map((x) => {
-        if(x.id === window.id) return window;
-        return x;
-      })
-    }
-  }
+    const updatedWindows = windows.map((win) =>
+      win.id === event.active.id
+        ? {
+            ...win,
+            position: {
+              x: win.position.x + event.delta.x,
+              y: win.position.y + event.delta.y,
+            },
+          }
+        : win
+    );
+    setWindows(updatedWindows);
+  };
 
   return (
     <div className="monitor flex items-center">
-      <DndContext collisionDetection={closestCenter} modifiers={[restrictToParentElement]} onDragEnd={handleDragEnd}>
+      <DndContext
+        collisionDetection={closestCenter}
+        modifiers={[restrictToParentElement, restrictToWindowEdges]}
+        onDragEnd={handleDragEnd}
+      >
         <div
           className="desktop"
-          style={{ position: "relative", width: "100%", height: "100vh" }} // Constrain parent
-          onClick={() => setSelectedDesktopShortcut("null")}
+          style={{ position: "relative", width: "100%", height: "100vh" }}
+          onClick={() => setSelectedDesktopShortcut("")}
         >
           <DesktopShortcut
             selectedShortcut={selectedDesktopShortcut}
@@ -77,12 +75,17 @@ export default function Home() {
           />
         </div>
         {windows.map((window) => (
-          <Window key={window.id} id={window.id} name={window.name} icon={window.icon} open={window.open} active={activeWindow === window.id} makeActive={() => setActiveWindow(window.id)} position={window.position} />
+          <Window
+            key={window.id}
+            {...window}
+            active={activeWindow === window.id}
+            makeActive={() => setActiveWindow(window.id)}
+          />
         ))}
-        <div className="taskbar flex flex-row flex-no-wrap justify-between">
+        <div className="taskbar flex flex-row justify-between">
           <div className="start"></div>
-          <div className="clock inline text-white text-bold flex flex-no-wrap items-center px-5 text-xl">
-            {retVal}
+          <div className="clock text-white text-bold flex items-center px-5 text-xl">
+            {formattedTime}
           </div>
         </div>
       </DndContext>
