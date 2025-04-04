c521b4bc904443ae3edb3798f724ea0090d76a36
Jake Busse <jakerbusse@gmail.com>
2025-04-04 10:35:57 -0500

    set redirect

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Site from "./Site";
-
-const el = document.getElementById("root");
-const root = ReactDOM.createRoot(el);
-
-root.render(<Site />);
